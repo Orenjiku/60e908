202aa26f53b7051d17e1992b21bd7de659bc6e24
William Chang <wdchang86@gmail.com>
2022-03-11 22:00:19 +0000

    Fixed nested state bug for setting conversations state in functions addNewConvo and addMessageToConversation.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -80,15 +80,17 @@ const Home = ({ user, logout }) => {
 
   const addNewConvo = useCallback(
     (recipientId, message) => {
-      const updatedConversations = [...conversations];
-      updatedConversations.forEach((convo) => {
+      setConversations(prev => prev.map((convo) => {
         if (convo.otherUser.id === recipientId) {
-          convo.messages.push(message);
-          convo.latestMessageText = message.text;
-          convo.id = message.conversationId;
+          const convoCopy = { ...convo };
+          convoCopy.messages.push(message);
+          convoCopy.latestMessageText = message.text;
+          convoCopy.id = message.conversationId;
+          return convoCopy;
+        } else {
+          return convo;
         }
-      });
-      setConversations(updatedConversations);
+      }));
     },
     [setConversations, conversations]
   );
@@ -107,14 +109,16 @@ const Home = ({ user, logout }) => {
         setConversations((prev) => [newConvo, ...prev]);
       }
 
-      const updatedConversations = [...conversations];
-      updatedConversations.forEach((convo) => {
+      setConversations(prev => prev.map((convo) => {
         if (convo.id === message.conversationId) {
-          convo.messages.push(message);
-          convo.latestMessageText = message.text;
+          const convoCopy = { ...convo };
+          convoCopy.messages.push(message);
+          convoCopy.latestMessageText = message.text;
+          return convoCopy;
+        } else {
+          return convo;
         }
-      });
-      setConversations(updatedConversations);
+      }));
     },
     [setConversations, conversations]
   );
